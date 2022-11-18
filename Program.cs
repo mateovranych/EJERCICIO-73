3fc1f60ed319cdfd5cc864b788da2949cf55b958
mateovranych <69678921+mateovranych@users.noreply.github.com>
2022-11-18 02:15:47 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO73
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena;
+
+            Queue colastring = new Queue();
+
+            Console.WriteLine("Ingrese la cadena para agregar a la cola:");
+
+            cadena = Console.ReadLine();
+
+            while (cadena != "salir")
+            {
+                colastring.Enqueue(cadena);
+                Console.WriteLine("Ingrese próxima cadena, ingrese ´salir´ para salir:");
+                cadena = Console.ReadLine();
+
+            }
+
+            if (cadena == "salir")
+            {
+                Console.WriteLine("La cola ingresada es:");
+            }
+
+            while (colastring.Count > 0)
+            {
+
+                string cad = (string)colastring.Dequeue();
+
+                Console.WriteLine(cad);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
